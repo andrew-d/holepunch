be1b9f20907d67f1122a40b36d603a88331ce960
Andrew Dunham <andrew@du.nham.ca>
2013-04-23 05:06:24 -0400

    Get the basics working

--- holepunch.go
+++ holepunch.go
@@ -5,12 +5,16 @@ import (
     "encoding/binary"
     "flag"
     "fmt"
+    "io"
     "log"
+    "math/rand"
+    "crypto/hmac"
+    "crypto/sha256"
     "net"
     "os"
-    "io"
     "strings"
     "time"
+    "encoding/hex"
 )
 
 const MAJOR_VER = 1
@@ -93,9 +97,15 @@ type PacketClient interface {
     Describe() string
 }
 
+// This interface represents a server - something that will accept clients.
+type GenericServer interface {
+    // Accept a single client and return it.
+    AcceptClient() (PacketClient, error)
+}
+
 // Global options
 var device = flag.String("d", "", "the tun/tap device to connect to")
-var password = flag.String("pass", "", "password for authentication")
+var password = flag.String("pass", "insecure", "password for authentication")
 
 // Client options
 var is_client = flag.Bool("c", false, "operate in client mode")
@@ -118,6 +128,9 @@ func main() {
     // Start by printing a header and parsing flags.
     flag.Usage = usage
     flag.Parse()
+
+    // Seed PRNG.
+    rand.Seed(time.Now().UTC().UnixNano())
 
     // Check client / server.
     if !*is_client && !*is_server {
@@ -160,33 +173,96 @@ func run_server() {
     // goroutine.  When a transport receives a connection from a new client,
     // it will kick off a new goroutine with the logic to authenticate and
     // then handle the connection.
+
+    log.Println("Starting TCP server...")
+    tcpServer, err := NewTCPPacketServer("")
+    if err != nil {
+        log.Printf("Error creating TCP server: %s\n", err)
+    } else {
+        startPacketServer(tcpServer, "TCP")
+        log.Println("Successfully started TCP server")
+    }
+
+    ch := make(chan bool)
+    <-ch
+}
+
+func startPacketServer(server GenericServer, method string) {
+    go (func() {
+        for {
+            client, _ := server.AcceptClient()
+            log.Printf("Accepted new client on %s transport\n", method)
+
+            go handle_new_client(&client)
+        }
+    })()
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func randomBytes(l int) []byte {
+    bytes := make([]byte, l)
+    for i := 0; i < l; i++ {
+        bytes[i] = charset[rand.Intn(len(charset))]
+    }
+
+    return []byte{'a', 'b', 'c', 'd'}
+    return bytes
 }
 
 // Authenticate and then handle the client.
-func handle_new_client() {
-    // First off, send the client a challenge.
-    //client.Send("challenge")
+func handle_new_client(client *PacketClient) {
+    // At the end of this function, we're to close the client.
+    defer (*client).Close()
+
+    // First off, send the client a challenge.  Generate a random nonce...
+    nonce := randomBytes(32)
+
+    // Send the challenge.
+    (*client).SendPacket(nonce)
 
     // Wait for one of three things:
     //  - Successful authentication
     //  - Unsuccessful authentication
     //  - Timeout
     res := make(chan []byte)
+    go (func() {
+        for {
+            pkt, err := (*client).GetPacket()
+            if err == nil {
+                res <- pkt
+                return
+            }
+        }
+    })()
+
+    // Calculate the proper response to the challenge.
+    hm := hmac.New(sha256.New, []byte(*password))
+    _, err := hm.Write(nonce)
+    if err != nil {
+        log.Printf("Error computing HMAC: %s\n", err)
+        return
+    }
+    expected := hex.EncodeToString(hm.Sum(nil))
+
     select {
     case resp := <-res:
-        // Validate auth.
-        _ = resp
-
         // If authentication failed...
-        if false {
+        if string(resp) != expected {
             log.Printf("Authentication failure")
+            (*client).SendPacket([]byte("failure"))
             return
+        } else {
+            log.Println("Authentication success!")
         }
     case <-time.After(10 * time.Second):
         // Timeout!
         log.Printf("Authentication timeout")
         return
     }
+
+    // Send a literal "success".
+    (*client).SendPacket([]byte("success"))
 
     // We've got a valid, authenticated client here.  Read packets from the
     // transport and dump them to our TUN/TAP interface, and read packets from
@@ -224,7 +300,7 @@ func run_client() {
         switch methods[i] {
         case "tcp":
             log.Printf("Trying TCP connection...")
-            curr, err = NewTCPPackettClient(args[0])
+            curr, err = NewTCPPacketClient(args[0])
         case "udp":
             log.Printf("Trying UDP connection...")
         case "icmp":
@@ -237,6 +313,44 @@ func run_client() {
             log.Println("Successfully created transport, starting authentication...")
         } else {
             log.Printf("Error: no transport returned (error: %s)\n", err)
+            continue
+        }
+
+        // Read a single packet.
+        challenge, err := curr.GetPacket()
+        if err != nil {
+            log.Printf("Error while reading authentication packet: %s\n", err)
+            continue
+        }
+
+        // Compute the HMAC of the challenge.
+        hm := hmac.New(sha256.New, []byte(*password))
+        _, err = hm.Write(challenge)
+        if err != nil {
+            log.Printf("Error computing HMAC: %s\n", err)
+            return
+        }
+        expected := hex.EncodeToString(hm.Sum(nil))
+
+        // Send to the server.
+        curr.SendPacket([]byte(expected))
+
+        // Read a packet - should contain a literal "success".
+        resp, err := curr.GetPacket()
+        if err != nil {
+            log.Printf("Error after authentication: %s", err)
+            continue
+        }
+        if bytes.Equal(resp, []byte("success")) {
+            log.Println("Authentication success!")
+            client = &curr
+            break
+        } else if bytes.Equal(resp, []byte("failure")) {
+            log.Println("Error with authentication - invalid password")
+            continue
+        } else {
+            log.Println("Error with authentication - got unexpected response")
+            continue
         }
     }
 
@@ -257,10 +371,17 @@ type TCPPacketClient struct {
     incoming chan []byte
 }
 
+// TODO: single struct with flag for which?
+type TCPPacketServer struct {
+    host     string
+    listener *net.Listener
+    incoming chan *TCPPacketClient
+}
+
 const TCP_PORT = 44461
 
 // Initializer.
-func NewTCPPackettClient(server string) (*TCPPacketClient, error) {
+func NewTCPPacketClient(server string) (*TCPPacketClient, error) {
     host := fmt.Sprintf("%s:%d", server, TCP_PORT)
 
     // Connect to this client.
@@ -270,59 +391,108 @@ func NewTCPPackettClient(server string) (*TCPPacketClient, error) {
         return nil, err
     }
 
+    ret := startClientWithConn(&conn, host)
+    return ret, nil
+}
+
+func startClientWithConn(conn *net.Conn, host string) *TCPPacketClient {
     // Make packet channel.
     incoming := make(chan []byte)
 
+    // Make the client.
+    ret := &TCPPacketClient{host, conn, incoming}
+
     // This goroutine handles reads that can time out.
+    go startTcpClientRecv(ret)
+
+    // Return client
+    return ret
+}
+
+func NewTCPPacketServer(bindTo string) (*TCPPacketServer, error) {
+    host := fmt.Sprintf("%s:%d", bindTo, TCP_PORT)
+
+    listener, err := net.Listen("tcp", host)
+    if err != nil {
+        log.Printf("Error listening on %s: %s\n", host, err)
+        return nil, err
+    }
+
+    // Create server.
+    ch := make(chan *TCPPacketClient)
+    server := &TCPPacketServer{bindTo, &listener, ch}
+
+    // This goroutine will accept new clients.
     go (func() {
         for {
-            var length int
-            lenb := make([]byte, binary.Size(length))
-
-            // Try to read the length.  On an EOF error, we exit this
-            // goroutine, and on all other errors just keep reading lengths.
-            _, err := conn.Read(lenb)
-            if err == io.EOF {
-                log.Println("EOF received on length read")
-                break
-            } else if err != nil {
-                log.Printf("Error while reading length: %s\n", err)
-                continue
-            }
-
-            // Decode the length.
-            err = binary.Read(bytes.NewBuffer(lenb), binary.LittleEndian, &length)
+            conn, err := listener.Accept()
             if err != nil {
-                log.Printf("Error decoding length: %s\n", err)
+                log.Printf("Error accepting new client: %s\n", err)
                 continue
             }
 
-            // Read this many bytes.
-            pkt := make([]byte, length)
-            _, err = conn.Read(pkt)
-            if err == io.EOF {
-                log.Println("EOF received on packet read")
-                break
-            } else if err != nil {
-                log.Printf("Error while reading packet: %s\n", err)
-                continue
-            }
+            // Create a new client.
+            client := startClientWithConn(&conn, "foobar")
 
-            // Got a packet - send to our channel.  Note that we will then
-            // loop around to the top again, and continue reading packets.
-            incoming <- pkt
+            // Send this client on our accepting channel.
+            ch <- client
         }
     })()
 
-    // All connected!  Return the transport.
-    ret := &TCPPacketClient{host, &conn, incoming}
-    return ret, nil
+    return server, nil
+}
+
+func (t *TCPPacketServer) AcceptClient() (PacketClient, error) {
+    client := <-t.incoming
+    return client, nil
+}
+
+func startTcpClientRecv(tcpClient *TCPPacketClient) {
+    for {
+        var length int16
+        lenb := make([]byte, 2)
+
+        // Try to read the length.  On an EOF error, we exit this
+        // goroutine, and on all other errors just keep reading lengths.
+        _, err := (*tcpClient.conn).Read(lenb)
+        if err == io.EOF {
+            log.Println("EOF received on length read")
+            break
+        } else if err != nil {
+            log.Printf("Error while reading length: %s\n", err)
+            continue
+        }
+
+        // Decode the length.
+        err = binary.Read(bytes.NewBuffer(lenb), binary.LittleEndian, &length)
+        if err != nil {
+            log.Printf("Error decoding length: %s\n", err)
+            continue
+        }
+
+        // Read this many bytes.
+        log.Printf("Reading %d bytes...\n", length)
+        pkt := make([]byte, length)
+        _, err = (*tcpClient.conn).Read(pkt)
+        if err == io.EOF {
+            log.Println("EOF received on packet read")
+            break
+        } else if err != nil {
+            log.Printf("Error while reading packet: %s\n", err)
+            continue
+        }
+
+        // Got a packet - send to our channel.  Note that we will then
+        // loop around to the top again, and continue reading packets.
+        tcpClient.incoming <- pkt
+    }
 }
 
 func (t *TCPPacketClient) SendPacket(pkt []byte) error {
     // Encode length of packet.
     buf := &bytes.Buffer{}
-    err := binary.Write(buf, binary.LittleEndian, len(pkt))
+    l := int16(len(pkt))
+    err := binary.Write(buf, binary.LittleEndian, l)
     if err != nil {
         return err
     }
