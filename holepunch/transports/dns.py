39b1dd4b2e33961994c0bf4595bbc074f078446b
Andrew Dunham <andrew@du.nham.ca>
2013-05-25 22:33:49 +0200

    Remove non-golang files

--- holepunch/transports/dns.py
+++
@@ -1,5 +0,0 @@
-def connect(address):
-    pass
-
-def listen(callback, *args, **kwargs):
-    pass
--- holepunch/tuntap/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if sys.platform == 'darwin':
-    from .darwin import TunTapDevice
-elif sys.platform.startswith('linux'):
-    from .linux import TunTapDevice
-else:
-    raise RuntimeError("Don't have a TUN/TAP interface for this platform!")
